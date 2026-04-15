ab1aaf16f095c33c67ba50918b7f2fabcd0ec884
yunjun <yunjun_5@naver.com>
2026-04-15 23:51:13 +0900

    v1.3

---
+++ src/main/java/com/example/member/dto/LoginRequest.java
@@ -0,0 +1,14 @@
+package com.example.member.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class LoginRequest {
+    @NotBlank
+    private String schoolEmail;
+    @NotBlank
+    private String password;
+}
